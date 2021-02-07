34e414ab3b9e7e789ffe42e67aba66504d0ac497
Alex-linkage <2542588064@qq.com>
2021-02-07 15:01:28 +0800

    dream server

---
+++ public/menu/index.js
@@ -0,0 +1,66 @@
+
+module.exports = [{
+    id: 0,
+    name: 'CSS',
+    path: '/',
+    code: '0000',
+    icon: 'el-icon-location',
+    children: [{
+        id: '00',
+        name: 'CSS简介',
+        path: '/cssIntroduce',
+        code: '0000-0000',
+        icon: 'el-icon-location'
+      },
+      {
+        id: '01',
+        name: 'BFC简述',
+        path: '/article/bfc',
+        code: '0000-0001',
+        icon: 'el-icon-location'
+      },
+      {
+        id: '02',
+        name: 'flex布局',
+        path: '/article/flex',
+        code: '0000-0002',
+        icon: 'el-icon-location'
+      },
+      {
+        id: '03',
+        name: '响应式布局',
+        path: '/article/responsive',
+        code: '0000-0003',
+        icon: 'el-icon-location'
+      }
+    ]
+  },
+  {
+    id: 1,
+    name: 'javascript',
+    path: '/',
+    code: '0001',
+    icon: 'el-icon-location',
+    children: [{
+      id: '10',
+      name: 'JS简介',
+      path: '/jsIntroduce',
+      code: '0001-0000',
+      icon: 'el-icon-location'
+    }]
+  },
+  {
+    id: 2,
+    name: '网络资料',
+    path: '/',
+    code: '0002',
+    icon: 'el-icon-location',
+    children: [{
+      id: '30',
+      name: '网络资料',
+      path: '/article/networkResource',
+      code: '0003-0000',
+      icon: 'el-icon-location'
+    }]
+  }
+]
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var menu = require('../public/menu/index.js')
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/getMenu', function(req, res, next) {
+  res.json({
+    status: 200,
+    menu
+  });
+});
+
+
+router.get('/article/:id', function(req, res, next) {
+  console.log(req.params.id)
+  const fileName = req.params.id
+  fs.readFile(`public/article/${fileName}.json`, 'utf-8', function(err, data) {
+    if (err) {
+      console.error(err);
+      res.json({
+        status: 500,
+        message: '文章不存在'
+      });
+    } else {
+      data = data.replace(/\r\n/g,"")
+      data = data.replace(/\n/g,"")
+      data = JSON.parse(data)
+      // console.log(data)
+      res.json({
+        status: 200,
+        data: data
+      });
+    }
+  });
+});
+
+module.exports = router;
