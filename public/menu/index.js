f887423b2fb9b2a8f462dbdd7482484ec7ef22a6
Alex-linkage <2542588064@qq.com>
2021-06-01 18:32:57 +0800

    结构调整

--- public/menu/index.js
+++ public/menu/index.js
@@ -1,73 +1,104 @@
-
-module.exports = [{
-    id: 0,
-    name: 'CSS',
-    path: '/',
-    code: '0000',
-    icon: 'el-icon-location',
-    children: [{
-        id: '00',
-        name: 'CSS简介',
-        path: '/cssIntroduce',
-        code: '0000-0000',
-        icon: 'el-icon-location'
-      },
-      {
-        id: '01',
-        name: 'BFC简述',
-        path: '/article/bfc',
-        code: '0000-0001',
-        icon: 'el-icon-location'
-      },
-      {
-        id: '02',
-        name: 'flex布局',
-        path: '/article/flex',
-        code: '0000-0002',
-        icon: 'el-icon-location'
-      },
-      {
-        id: '03',
-        name: '响应式布局',
-        path: '/article/responsive',
-        code: '0000-0003',
-        icon: 'el-icon-location'
-      }
-    ]
-  },
+module.exports = [
+  // {
+  //   id: 'html',
+  //   name: 'HTML',
+  //   path: '/',
+  //   code: 'html',
+  //   icon: 'el-icon-location',
+  //   children: [{
+  //       id: 'html-00',
+  //       name: 'html简介',
+  //       path: '/article/html',
+  //       code: 'html-0000',
+  //       icon: 'el-icon-location'
+  //     }
+  //   ]
+  // },
+  // {
+  //   id: 0,
+  //   name: 'CSS',
+  //   path: '/',
+  //   code: '0000',
+  //   icon: 'el-icon-location',
+  //   children: [{
+  //       id: '00',
+  //       name: 'CSS简介',
+  //       path: '/cssIntroduce',
+  //       code: '0000-0000',
+  //       icon: 'el-icon-location'
+  //     },
+  //     {
+  //       id: '01',
+  //       name: 'BFC简述',
+  //       path: '/article/bfc',
+  //       code: '0000-0001',
+  //       icon: 'el-icon-location'
+  //     },
+  //     {
+  //       id: '02',
+  //       name: 'flex布局',
+  //       path: '/article/flex',
+  //       code: '0000-0002',
+  //       icon: 'el-icon-location'
+  //     },
+  //     {
+  //       id: '03',
+  //       name: '响应式布局',
+  //       path: '/article/responsive',
+  //       code: '0000-0003',
+  //       icon: 'el-icon-location'
+  //     }
+  //   ]
+  // },
   {
     id: 1,
     name: 'javascript',
     path: '/',
     code: '0001',
     icon: 'el-icon-location',
-    children: [{
-      id: '10',
-      name: 'JS简介',
-      path: '/jsIntroduce',
-      code: '0001-0000',
-      icon: 'el-icon-location'
-    },
-    {
-      id: '11',
-      name: 'JS数组篇',
-      path: '/article/js_array',
-      code: '0001-0001',
-      icon: 'el-icon-location'
-    }]
+    children: [
+      // {
+      //   id: '10',
+      //   name: 'JS简介',
+      //   path: '/jsIntroduce',
+      //   code: '0001-0000',
+      //   icon: 'el-icon-location'
+      // },
+      {
+        id: '11',
+        name: 'JS数组篇',
+        path: '/article/js_array',
+        code: '0001-0001',
+        icon: 'el-icon-location'
+      }
+    ]
   },
   {
-    id: 2,
-    name: '网络资料',
+    id: 3,
+    name: 'Ajax',
     path: '/',
-    code: '0002',
+    code: '0003',
     icon: 'el-icon-location',
     children: [{
       id: '30',
-      name: '网络资料',
-      path: '/article/networkResource',
+      name: 'Ajax详解',
+      path: '/article/ajax',
       code: '0003-0000',
       icon: 'el-icon-location'
     }]
-  }
+  },
+  // {
+  //   id: 2,
+  //   name: '网络资料',
+  //   path: '/',
+  //   code: '0002',
+  //   icon: 'el-icon-location',
+  //   children: [{
+  //     id: '20',
+  //     name: '网络资料',
+  //     path: '/article/networkResource',
+  //     code: '0002-0000',
+  //     icon: 'el-icon-location'
+  //   }]
+  // }
 ]
