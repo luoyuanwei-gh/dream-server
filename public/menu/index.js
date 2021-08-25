8d1671437004c8b5d0c35d596e493d21fe479ed2
Alex-linkage <2542588064@qq.com>
2021-08-25 13:02:46 +0800

    菜单调整

--- public/menu/index.js
+++ public/menu/index.js
@@ -14,42 +14,50 @@ module.exports = [
   //     }
   //   ]
   // },
-  // {
-  //   id: 0,
-  //   name: 'CSS',
-  //   path: '/',
-  //   code: '0000',
-  //   icon: 'el-icon-location',
-  //   children: [{
-  //       id: '00',
-  //       name: 'CSS简介',
-  //       path: '/cssIntroduce',
-  //       code: '0000-0000',
-  //       icon: 'el-icon-location'
-  //     },
-  //     {
-  //       id: '01',
-  //       name: 'BFC简述',
-  //       path: '/article/bfc',
-  //       code: '0000-0001',
-  //       icon: 'el-icon-location'
-  //     },
-  //     {
-  //       id: '02',
-  //       name: 'flex布局',
-  //       path: '/article/flex',
-  //       code: '0000-0002',
-  //       icon: 'el-icon-location'
-  //     },
-  //     {
-  //       id: '03',
-  //       name: '响应式布局',
-  //       path: '/article/responsive',
-  //       code: '0000-0003',
-  //       icon: 'el-icon-location'
-  //     }
-  //   ]
-  // },
+  {
+    id: 0,
+    name: 'CSS',
+    path: '/',
+    code: '0000',
+    icon: 'el-icon-location',
+    children: [
+      {
+        id: 'css_demo',
+        name: 'CSS简介',
+        path: '/css_demo',
+        code: '0000-0000',
+        icon: 'el-icon-location'
+      }
+      // {
+      //   id: '00',
+      //   name: 'CSS简介',
+      //   path: '/cssIntroduce',
+      //   code: '0000-0000',
+      //   icon: 'el-icon-location'
+      // },
+      // {
+      //   id: '01',
+      //   name: 'BFC简述',
+      //   path: '/article/bfc',
+      //   code: '0000-0001',
+      //   icon: 'el-icon-location'
+      // },
+      // {
+      //   id: '02',
+      //   name: 'flex布局',
+      //   path: '/article/flex',
+      //   code: '0000-0002',
+      //   icon: 'el-icon-location'
+      // },
+      // {
+      //   id: '03',
+      //   name: '响应式布局',
+      //   path: '/article/responsive',
+      //   code: '0000-0003',
+      //   icon: 'el-icon-location'
+      // }
+    ]
+  },
   {
     id: 1,
     name: 'javascript',
